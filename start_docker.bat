f12639e007233198fb1f72ba04aa7389268e6463
pbyrne84 <pbyrne84@gmail.com>
2023-02-08 13:55:56 +0000

    Create local Ubuntu docker image for manual testing purposes as relyi… (#11)
    * Create local Ubuntu docker image for manual testing purposes as relying on Github actions has a very poor feedback loop
    
    * add fat jar and linux executable
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- start_docker.bat
+++ start_docker.bat
@@ -1,6 +1,6 @@
-docker build -t amazon_vm .
+docker build -t lib_resolver_vm .
 rem pwd seems to break on background updates, hopefully this enables a stable commit  :(
 set "currentDir=%cd%"
 echo %currentDir%
-docker run --privileged  --name amazon_compile --mount src="%currentDir%",target=/root/project_mount,type=bind -t -d amazon_vm
-docker exec -it amazon_compile bash
+docker run --name lib_resolver_compile --mount src="%currentDir%",target=/root/project_mount,type=bind -t -d lib_resolver_vm  || exit /b
+docker exec -it lib_resolver_compile bash
